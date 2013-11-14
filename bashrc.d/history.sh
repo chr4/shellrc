8086602edce79b20e411fdb5df1ab051da90bb17
Chris Aumann <me@chr4.org>
2013-11-14 17:36:37 +0100

    add some comments to bash history settings

--- bashrc.d/history.sh
+++ bashrc.d/history.sh
@@ -1,6 +1,14 @@
 # Enable history appending instead of overwriting.
 shopt -s histappend
+
+# the maximum number of lines contained in the history file
 HISTFILESIZE=100000
+
+# the  number of commands to remember in the command history
 HISTSIZE=100000
+
+# ignore lines starting with a whitespace, as well as duplicates
 HISTCONTROL=ignoreboth
+
+# use shared history file with zsh
 HISTFILE=~/.history
