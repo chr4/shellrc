4f2b115815632589fd16195b7953993ea7879671
Chris Aumann <me@chr4.org>
2013-11-14 23:59:46 +0100

    remove #! lines for sourced files

--- bash_profile
+++ bash_profile
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 if [ -f ~/.bashrc ]; then
   source ~/.bashrc
 fi
--- rc.d/homebrew.sh
+++ rc.d/homebrew.sh
@@ -1,4 +1,2 @@
-#!/usr/bin/env bash
-
 # do not talk to github on every search (doesn't work?)
 HOMEBREW_NO_GITHUB_API=true
