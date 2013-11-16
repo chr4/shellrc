9dc211bb6e1ec9f423155138d071c96dd697ded6
Chris Aumann <me@chr4.org>
2013-11-16 12:22:45 +0100

    merge bindkey files

--- zshrc.d/bindkey.zsh
+++ zshrc.d/bindkey.zsh
@@ -1,3 +1,6 @@
+# start in vi mode
+bindkey -v
+
 # setup keys bindings for every mode
 for mode in vicmd viins emacs; do
   # use up/down arrows to search history
@@ -18,3 +21,27 @@ for mode in vicmd viins emacs; do
   zle -N edit-command-line
   bindkey -M $mode '^V' edit-command-line
 done
+
+# yank
+bindkey -M vicmd 'YY' vi-yank-whole-line
+bindkey -M vicmd 'Y'  vi-yank-whole-line
+
+# bind undo and redo keys
+bindkey -M vicmd 'U' undo
+bindkey -M vicmd '^R' redo
+
+# use page-up/down to search history (insert and command mode)
+bindkey -M viins '^[[I' history-beginning-search-backward
+bindkey -M viins '^[[G' history-beginning-search-forward
+bindkey -M vicmd '^[[I' history-beginning-search-backward
+bindkey -M vicmd '^[[G' history-beginning-search-forward
+
+# use up/down arrows to search history (insert and command mode)
+bindkey -M viins '^[[A' history-beginning-search-backward
+bindkey -M viins '^[[B' history-beginning-search-forward
+bindkey -M vicmd '^[[A' history-beginning-search-backward
+bindkey -M vicmd '^[[B' history-beginning-search-forward
+
+# when in command mode, also allow history search with j and k
+bindkey -M vicmd 'K' history-beginning-search-backward
+bindkey -M vicmd 'J' history-beginning-search-forward
