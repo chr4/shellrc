2e462268d50cca8c2077272af13adc44a1ae2e55
Chris Aumann <me@chr4.org>
2013-11-17 22:41:18 +0100

    use local color variables for bash git prompt

--- bashrc.d/prompt.bash
+++ bashrc.d/prompt.bash
@@ -1,31 +1,36 @@
-# colors
-black=$'\e[00;30m'
-red=$'\e[00;31m'
-green=$'\e[00;32m'
-yellow=$'\e[00;33m'
-blue=$'\e[00;34m'
-magenta=$'\e[00;35m'
-cyan=$'\e[00;36m'
-white=$'\e[00;37m'
-none=$'\e[00;00m'
-
 function parse_git_branch_full_ref() {
   (git symbolic-ref -q HEAD || git name-rev --name-only --no-undefined --always HEAD) 2> /dev/null
 }
 
-function parse_git_state() {
-  local git_state=""
+# if inside a git repository, print its branch and state
+function git_prompt_string() {
+  # colors
+  local black=$'\e[00;30m'
+  local red=$'\e[00;31m'
+  local green=$'\e[00;32m'
+  local yellow=$'\e[00;33m'
+  local blue=$'\e[00;34m'
+  local magenta=$'\e[00;35m'
+  local cyan=$'\e[00;36m'
+  local white=$'\e[00;37m'
+  local none=$'\e[00;00m'
 
+  local git_state=""
+  local git_where=$(parse_git_branch_full_ref)
+
+  # merge
   local git_dir="$(git rev-parse --git-dir 2> /dev/null)"
   if [ -n $git_dir ] && test -r $git_dir/MERGE_HEAD; then
     git_state=${git_state}${red}⚡︎
   fi
 
+  # untracked files
   local num_untracked=$(git ls-files --other --exclude-standard 2> /dev/null |wc -l |tr -d ' ')
   if [ "$num_untracked" -gt 0 ]; then
     git_state=${git_state}${red}…
   fi
 
+  # staged files
   local num_staged=$(git diff --cached --name-only 2> /dev/null |wc -l |tr -d ' ')
   if [ "$num_staged" -gt 0 ]; then
     git_state=${git_state}${yellow}✗
@@ -38,15 +43,8 @@ function parse_git_state() {
     git_state=${green}✔
   fi
 
-  echo -en ${git_state}${none}
-}
-
-# If inside a git repository, print its branch and state
-function git_prompt_string() {
-  local git_where=$(parse_git_branch_full_ref)
-
   if [ -n "$git_where" ]; then
-    echo -en "(${green}${git_where#refs/heads/}${none}|$(parse_git_state)) "
+    echo -en "(${green}${git_where#refs/heads/}${none}|${git_state}${none}) "
   fi
 }
 
