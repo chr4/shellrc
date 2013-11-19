0006da476a90e5d1be5f154df07e651edbad01a2
Chris Aumann <me@chr4.org>
2013-11-19 01:10:24 +0100

    set window title [zsh]

---
+++ zshrc.d/set-window-title.zsh
@@ -0,0 +1,7 @@
+function precmd() {
+  echo -ne "\033]0;${USER}@$(hostname) | $(basename $PWD)\007"
+}
+
+function preexec() {
+  echo -ne "\033]0;${USER}@$(hostname) | $(basename $PWD) | $1\007"
+}
