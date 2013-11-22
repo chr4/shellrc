706ff847fb69bbe46e4b67badd04a788f0646c5b
Chris Aumann <me@chr4.org>
2013-11-22 12:13:54 +0100

    merge set-window-title to xterminals

--- rc.d/xterminals.sh
+++ rc.d/xterminals.sh
@@ -1,8 +1,17 @@
 case ${TERM} in
   xterm*|rxvt*|Eterm|aterm|kterm|gnome*|interix)
-    PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME%%.*}:${PWD/$HOME/~}\007"'
-    ;;
-  screen)
-    PROMPT_COMMAND='echo -ne "\033_${USER}@${HOSTNAME%%.*}:${PWD/$HOME/~}\033\\"'
-    ;;
+    if [ -n "$ZSH_VERSION" ]; then
+      function precmd() {
+        echo -ne "\033]0;$(hostname) | ${PWD/$HOME/~}\007"
+      }
+
+      function preexec() {
+        echo -ne "\033]0;$(hostname) | ${PWD/$HOME/~} | $1\007"
+      }
+    fi
+
+    if [ -n "$BASH" ]; then
+      PROMPT_COMMAND='echo -ne "\033]0;${HOSTNAME%%.*} | ${PWD/$HOME/~}\007"'
+    fi
+  ;;
 esac
--- zshrc.d/set-window-title.zsh
+++
@@ -1,7 +0,0 @@
-function precmd() {
-  echo -ne "\033]0;$(hostname) | $(basename $PWD)\007"
-}
-
-function preexec() {
-  echo -ne "\033]0;$(hostname) | $(basename $PWD) | $1\007"
-}
