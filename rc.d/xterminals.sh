365ac6a773be9e1cdbf3a42d97b5133cf630ac7c
Chris Aumann <me@chr4.org>
2013-11-23 20:37:04 +0100

    fix hostname, use basename $PWD [xterminals]
    fix hostname on broken networks

--- rc.d/xterminals.sh
+++ rc.d/xterminals.sh
@@ -1,17 +1,20 @@
 case ${TERM} in
   xterm*|rxvt*|Eterm|aterm|kterm|gnome*|interix)
     if [ -n "$ZSH_VERSION" ]; then
+      # there's no HOSTNAME variable on zsh
+      HOSTNAME=$(hostname)
+
       function precmd() {
-        echo -ne "\033]0;$(hostname) | ${PWD/$HOME/~}\007"
+        echo -ne "\033]0;${HOSTNAME%%.*} | $(basename $PWD)\007"
       }
 
       function preexec() {
-        echo -ne "\033]0;$(hostname) | ${PWD/$HOME/~} | $1\007"
+        echo -ne "\033]0;${HOSTNAME%%.*} | $(basename $PWD) | $1\007"
       }
     fi
 
     if [ -n "$BASH" ]; then
-      PROMPT_COMMAND='echo -ne "\033]0;${HOSTNAME%%.*} | ${PWD/$HOME/~}\007"'
+      PROMPT_COMMAND='echo -ne "\033]0;${HOSTNAME%%.*} | $(basename $PWD)\007"'
     fi
   ;;
 esac
