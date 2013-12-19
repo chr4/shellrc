1de979b5a56304782c368df8000473b9be33e70e
Chris Aumann <me@chr4.org>
2013-12-19 15:35:30 +0100

    Use buildin prompt signs

--- zshrc.d/prompt.zsh
+++ zshrc.d/prompt.zsh
@@ -2,9 +2,9 @@ autoload -Uz colors && colors
 
 # set colorful prompt (PS1 doesn't support the color variables set above)
 if [[ ${EUID} == 0 ]]; then
-  PROMPT='%{$fg[white]%}%m %{$fg[blue]%}%1~%{$reset_color%}$(__git_ps1 " (%s)") %{$fg[red]%}#%{$reset_color%} '
+  PROMPT='%{$fg[white]%}%m %{$fg[blue]%}%1~%{$reset_color%}$(__git_ps1 " (%s)") %{$fg[red]%}%#%{$reset_color%} '
 else
-  PROMPT='%{$fg[white]%}%m %{$fg[blue]%}%1~%{$reset_color%}$(__git_ps1 " (%s)") %{$fg[white]%}%%%{$reset_color%} '
+  PROMPT='%{$fg[white]%}%m %{$fg[blue]%}%1~%{$reset_color%}$(__git_ps1 " (%s)") %{$fg[white]%}%#%{$reset_color%} '
 fi
 
 RPROMPT='$(vi_mode_prompt_info) $(exit_code_prompt_info)'
